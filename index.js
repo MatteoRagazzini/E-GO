3d6b3b1d1acd96aefbc1bab433d41246c73a7b24
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2022-11-09 22:06:07 +0100

    added default 404 error code

--- index.js
+++ index.js
@@ -43,13 +43,13 @@ app.use(bodyParser.json());
 
 app.use('/static', express.static(__dirname + '/public'));
 
-// app.use(function(req, res) {
-//     res.status(404).send({url: req.originalUrl + ' not found'})
-// });
-
 require('./src/routes/router')(app)
 require('./src/routes/auth.routes')(app);
 require('./src/routes/user.routes')(app);
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+});
 
 
 app.listen(port, () => {

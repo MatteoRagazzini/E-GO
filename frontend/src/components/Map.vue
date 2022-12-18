5b6b4e8a0413e4079986cf3829343c3cc949140b
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2022-12-18 13:46:03 +0100

    station card realized with object station as a prop

--- frontend/src/components/Map.vue
+++ frontend/src/components/Map.vue
@@ -6,89 +6,94 @@
   <div ref="mapDiv" style="width:100%; height:80vh"/>
   <StationCard
     v-model="state.dialog"
-    :id="state.dialogText"/>
+    :station ="state.station"/>
 </template>
-
 
 
 <!--This is a component realized with the composition API. Which means no return.-->
 <!--I would like to write this component like a normal vue component because it's more readable I think-->
 <script setup>
 
-  import StationService from "@/services/station.service";
-  import {computed, onMounted, reactive, ref, watch} from "vue";
-  import {useGeolocation} from "@/map/GeolocationFuctions";
-  import StationCard from "@/components/StationCard";
+import StationService from "@/services/station.service";
+import {computed, onMounted, reactive, ref, watch} from "vue";
+import {useGeolocation} from "@/map/GeolocationFuctions";
+import StationCard from "@/components/StationCard";
 
-  const {coords} = useGeolocation()
-    const currPos = computed(() => ({
-      lat: coords.value.latitude,
-      lng: coords.value.longitude
-    }))
+const {coords} = useGeolocation()
+const currPos = computed(() => ({
+  lat: coords.value.latitude,
+  lng: coords.value.longitude
+}))
 
-    const state = reactive({dialogText:"", dialog:false});
-
-    const stationProp = defineProps({
-      id: String,
-    })
-
-    const mapDiv = ref(null)
-    let map = ref(null)
-
-    onMounted(async () => {
-      map.value = new google.maps.Map(mapDiv.value, {
-        center: currPos.value,
-        zoom: 13,
-        mapId: "16c023e99af33056",
-      })
-    })
-
-  StationService.getStation().then(
-    (response) => {
-      var stations = response.data
-      // const availabilityTag = document.createElement("div");
-
-      stations.forEach(station => {
-        // if (station.usedSpaces !== station.maxSpaces) {
-        //   availabilityTag.className = "availability-tag available";
-        // } else {
-        //   availabilityTag.className = "availability-tag unavailable";
-        // }
-        //
-        // availabilityTag.textContent = station.usedSpaces + "/" + station.maxSpaces;
-        //
-        // availabilityTag.id = station._id
-        new google.maps.Marker({
-          position: new google.maps.LatLng(station.latitude, station.longitude),
-          // content: availabilityTag,
-          label: ""+ station.usedSpaces,
-          map:map.value
-        }).addListener("click", () => {
-          state.dialog = true
-          state.dialogText = station._id
-        })
-      })
-    })
-
-  var locationMarker = null;
-  var locationMarkerIsSet = false;
-
-  watch([currPos], () => {
-    map.value.setCenter(currPos.value)
-    map.value.setZoom(15)
-    if(locationMarkerIsSet){
-      console.log("inside the location isSet")
-      locationMarker.setMap(null);
+const state = reactive({
+    dialogText: "",
+    dialog: false,
+    station: {
+      id: 1,
+      ratings: 4.5,
+      title: "Station example",
+      reviews: 400,
     }
-    locationMarkerIsSet = true;
-    locationMarker= new google.maps.Marker({
-      position: new google.maps.LatLng(currPos.value),
-      map:map.value,
-      icon: {
-        url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-      }
+  }
+);
+
+const mapDiv = ref(null)
+let map = ref(null)
+
+onMounted(async () => {
+  map.value = new google.maps.Map(mapDiv.value, {
+    center: currPos.value,
+    zoom: 13,
+    mapId: "16c023e99af33056",
+  })
+})
+
+StationService.getStation().then(
+  (response) => {
+    var stations = response.data
+    // const availabilityTag = document.createElement("div");
+
+    stations.forEach(station => {
+      // if (station.usedSpaces !== station.maxSpaces) {
+      //   availabilityTag.className = "availability-tag available";
+      // } else {
+      //   availabilityTag.className = "availability-tag unavailable";
+      // }
+      //
+      // availabilityTag.textContent = station.usedSpaces + "/" + station.maxSpaces;
+      //
+      // availabilityTag.id = station._id
+      new google.maps.Marker({
+        position: new google.maps.LatLng(station.latitude, station.longitude),
+        // content: availabilityTag,
+        label: "" + station.usedSpaces,
+        map: map.value
+      }).addListener("click", () => {
+        state.dialog = true
+        state.dialogText = station._id
+      })
     })
   })
+
+var locationMarker = null;
+var locationMarkerIsSet = false;
+
+watch([currPos], () => {
+  map.value.setCenter(currPos.value)
+  map.value.setZoom(15)
+  if (locationMarkerIsSet) {
+    console.log("inside the location isSet")
+    locationMarker.setMap(null);
+  }
+  locationMarkerIsSet = true;
+  locationMarker = new google.maps.Marker({
+    position: new google.maps.LatLng(currPos.value),
+    map: map.value,
+    icon: {
+      url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+    }
+  })
+})
 
 </script>
 
--- frontend/src/components/StationCard.vue
+++ frontend/src/components/StationCard.vue
@@ -1,21 +1,107 @@
 <template>
   <v-dialog>
-    <v-card>
+<!--    <v-card>-->
+<!--      <v-card-text>-->
+<!--        id: {{id}}-->
+<!--      </v-card-text>-->
+<!--    </v-card>-->
+    <v-card
+      :loading="loading"
+      class="mx-auto my-12"
+      max-width="374"
+    >
+      <template v-slot:loader="{ isActive }">
+        <v-progress-linear
+          :active="isActive"
+          color="deep-purple"
+          height="4"
+          indeterminate
+        ></v-progress-linear>
+      </template>
+
+      <v-card-item>
+        <v-card-title>{{ this.station.title }}</v-card-title>
+      </v-card-item>
+
       <v-card-text>
-        id: {{id}}
+        <v-row
+          align="center"
+          class="mx-0"
+        >
+          <v-rating
+            :model-value="this.station.ratings"
+            color="amber"
+            density="compact"
+            half-increments
+            readonly
+            size="small"
+          ></v-rating>
+
+          <div class="text-grey ms-4">
+            {{this.station.ratings}} {{this.station.reviews}}
+          </div>
+        </v-row>
+
+        <div class="my-4 text-subtitle-1">
+          $ • Italian, Cafe
+        </div>
+
+        <div>Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.</div>
       </v-card-text>
+
+      <v-divider class="mx-4 mb-1"></v-divider>
+
+      <v-card-title>Tonight's availability</v-card-title>
+
+      <div class="px-4">
+        <v-chip-group v-model="selection">
+          <v-chip>5:30PM</v-chip>
+
+          <v-chip>7:30PM</v-chip>
+
+          <v-chip>8:00PM</v-chip>
+
+          <v-chip>9:00PM</v-chip>
+        </v-chip-group>
+      </div>
+
+      <v-card-actions>
+        <v-btn
+          color="deep-purple-lighten-2"
+          variant="text"
+          @click="reserve"
+        >
+          Reserve
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
-<script setup>
+<script>
+export default {
+  data: () => ({
+    loading: false,
+    selection: 1,
+  }),
 
-const stationProp = defineProps({
-    id: String,
-  })
+  methods: {
+    reserve () {
+      this.loading = true
 
+      setTimeout(() => (this.loading = false), 2000)
+    },
+  },
+  props:{
+    station:{
+      id: String,
+      ratings: Number,
+      title:String,
+      reviews: Number
+    }
+  }
+}
 </script>
-
 
 <style>
 </style>

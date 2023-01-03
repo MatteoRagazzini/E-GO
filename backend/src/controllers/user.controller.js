3c63492d4c0f700f23106310f524d6197bae16b9
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2023-01-03 11:47:38 +0100

    refactor of mongoose query as promise chain

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -18,6 +18,40 @@ exports.moderatorBoard = (req, res) => {
     res.status(200).send("Moderator Content.");
 };
 
+function findUser(req) {
+    return new Promise((resolve, reject) => {
+        User.findById(req.body.user_id, function (err, user) {
+            if (err)
+                reject(err);
+            else {
+                if (user == null) reject("User not found")
+                else resolve(user)
+            }
+        })
+    })
+}
+
+function perfmormOperation(user, req, operation, vehicle = {}) {
+    let vehicleToRemove = {}
+    return new Promise((resolve, reject) => {
+        if (operation === 'push') user.vehicles.push(vehicle);
+        else if (operation === 'pull') {
+            vehicleToRemove = user.vehicles.find(v => v._id = req.body.vehicle_id);
+            if (vehicleToRemove == null) reject('vehicle not found')
+            user.vehicles.splice(vehicleToRemove, 1)
+        }
+        else{
+            reject('operation unknown')
+        }
+        user.save().then(
+            resolve("operation performed")
+        ).catch(error => {
+                reject(error)
+            }
+        )
+    })
+}
+
 exports.addVehicle = (req, res) => {
 
     const newVehicle = {
@@ -29,24 +63,9 @@ exports.addVehicle = (req, res) => {
         isCurrent: req.body.isCurrent
     };
 
-    User.findOneAndUpdate({_id: req.body.user_id}, {
-        $push: {
-            'vehicles':
-            newVehicle
-        }
-    }, {new: true}, function (err, user) {
-        if (err)
-            res.send(err);
-        else {
-            if (user == null) {
-                res.status(404).send({
-                    description: 'user not found'
-                });
-            } else {
-                res.status(200).send("vehicle successfully added to the user");
-            }
-        }
-    });
+    findUser(req)
+        .then(user => perfmormOperation(user, req, 'push', newVehicle))
+        .then(result => res.status(200).send(result))
 }
 
 exports.updateVehicle = (req, res) => {
@@ -75,7 +94,7 @@ exports.updateVehicle = (req, res) => {
 
                     user.save().then(
                         res.status(200).send("vehicle successfully updated")
-                    ).catch(e=>{
+                    ).catch(e => {
                             res.status(404).send({
                                 description: 'save not completed'
                             });
@@ -108,11 +127,11 @@ exports.removeVehicle = (req, res) => {
                     user.vehicles.splice(index, 1)
                     user.save().then(
                         res.status(200).send("vehicle successfully removed")
-                    ).catch(e=>{
+                    ).catch(e => {
                             res.status(404).send({
                                 description: 'save not completed'
                             });
-                    }
+                        }
                     )
 
                 }
@@ -122,25 +141,30 @@ exports.removeVehicle = (req, res) => {
 }
 
 exports.addFavouriteStation = (req, res) => {
-    User.findOneAndUpdate({_id: req.body.user_id}, {
-        $push: {
-            'favouriteStations':
-            req.body.station_id
-        }
-    }, {new: true}, function (err, user) {
-        if (err)
-            res.send(err);
-        else {
-            if (user == null) {
-                res.status(404).send({
-                    description: 'user not found'
-                });
-            } else {
-                console.log("station successfully added to the user")
-                res.status(200).send("station successfully added to the user");
-            }
-        }
-    });
+
+    findUser(req)
+        .then(user => console.log(res))
+        .catch(err => console.log(err))
+
+    // User.findOneAndUpdate({_id: req.body.user_id}, {
+    //     $push: {
+    //         'favouriteStations':
+    //         req.body.station_id
+    //     }
+    // }, {new: true}, function (err, user) {
+    //     if (err)
+    //         res.send(err);
+    //     else {
+    //         if (user == null) {
+    //             res.status(404).send({
+    //                 description: 'user not found'
+    //             });
+    //         } else {
+    //             console.log("station successfully added to the user")
+    //             res.status(200).send("station successfully added to the user");
+    //         }
+    //     }
+    // });
 }
 
 exports.removeFavouriteStation = (req, res) => {
@@ -214,3 +238,6 @@ exports.connect = (req, res) => {
         }
     });
 }
+
+
+

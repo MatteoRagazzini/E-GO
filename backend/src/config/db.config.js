9f6bdef76a7b0251186a43b1187c5f9f33a87167
Matteo Ragazzini <matteo.ragazzini5@studio.unibo.it>
2023-01-15 10:34:43 +0100

    working docker

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -1,10 +1,10 @@
-const {
-    MONGODB_USER,
-    MONGODB_PASSWORD,
-    MONGODB_HOST,
-    MONGODB_DOCKER_PORT,
-    MONGODB_DATABASE,
-} = process.env;
+// const {
+//     MONGODB_USER,
+//     MONGODB_PASSWORD,
+//     MONGODB_HOST,
+//     MONGODB_DOCKER_PORT,
+//     MONGODB_DATABASE,
+// } = process.env;
 
 // MONGODB_USER=root
 // MONGODB_PASSWORD=123456
@@ -13,7 +13,14 @@ const {
 // MONGODB_DOCKER_PORT=27017
 // MONGODB_HOST=mongodb
 
+const MONGODB_HOST = process.env.MONGODB_HOST || "localhost";
+
+const MONGODB_DOCKER_PORT = process.env.MONGODB_DOCKER_PORT || 27017;
+const MONGODB_DATABASE = process.env.MONGODB_DATABASE || "bezkoder_db";
+
+
 module.exports = {
     // url: `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}:${MONGODB_DOCKER_PORT}/${MONGODB_DATABASE}?authSource=admin`,
-    url: `mongodb://root:123456@mongodb:27017/bezkoder_db?authSource=admin`,
+    // url: `mongodb://root:123456@mongodb:27017/bezkoder_db?authSource=admin`,
+    url: `mongodb://${MONGODB_HOST}:${MONGODB_DOCKER_PORT}/${MONGODB_DATABASE}`
 };
